0c5fdecb07a546616113804caf3c99cc9b2c70a6
Ahmed Emad <ahmedemad01100001@gmail.com>
2025-09-29 19:34:46 +0300

    added chat feature

--- lib/pages/profile_details_from_chat_page.dart
+++ lib/pages/profile_details_from_chat_page.dart
@@ -4,163 +4,123 @@ import 'package:sharek/Colors/Colors.dart';
 
 class ProfileDetailsFromChat extends StatelessWidget {
   ProfileDetailsFromChat({super.key});
-  // this is for test only
-  var notificationVal = true;
+
+  // reactive values using GetX
+  final RxBool notificationVal = true.obs;
+  final RxBool pinChatVal = false.obs;
 
   @override
   Widget build(BuildContext context) {
-    final args = Get.arguments; // dynamic!!!!!!!!
+    final args = Get.arguments;
     final String name = args[0];
     final Widget profilePic = args[1];
+
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        //mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SizedBox(),
               Column(
                 children: [
                   profilePic,
                   Text(
                     name,
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ],
               ),
-              SizedBox(),
             ],
           ),
-          SizedBox(height: 32),
-          Container(
-            height: 100,
-            width: double.infinity,
-            margin: const EdgeInsets.all(5),
-            padding: const EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: mainColor,
-              borderRadius: BorderRadius.circular(15),
-            ),
-
-            child: Column(
-              children: [
-                Row(
-                  spacing: 8,
-                  children: [
-                    Icon(Icons.notifications, color: Colors.white, size: 32),
-                    Text(
-                      "Mute notification",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Expanded(child: SizedBox()),
-                    Switch(
-                      value: notificationVal,
-                      onChanged: (notificationVal) {},
-                      activeColor: Colors.white, // لون الدائرة (thumb) وهي On
-                      activeTrackColor: secondaryColor, // لون الخلفية وهي On
-                      inactiveThumbColor:
-                          Colors.grey.shade400, // لون الدائرة وهي Off
-                      inactiveTrackColor:
-                          Colors.grey.shade300, // لون الخلفية وهي Off
-                      trackOutlineColor: MaterialStateProperty.all(
-                        Colors.transparent,
-                      ),
-                    ),
-                  ],
+          const SizedBox(height: 32),
+          // mute notifications
+          Obx(
+            () => settingsTile(
+              icon: Icons.notifications,
+              text: "Mute notification",
+              trailing: Switch(
+                value: notificationVal.value,
+                onChanged: (v) => notificationVal.value = v,
+                activeColor: Colors.white,
+                activeTrackColor: secondaryColor,
+                inactiveThumbColor: Colors.grey.shade400,
+                inactiveTrackColor: Colors.grey.shade300,
+                trackOutlineColor: MaterialStateProperty.all(
+                  Colors.transparent,
                 ),
-              ],
+              ),
             ),
           ),
-
-          Container(
-            height: 100,
-            width: double.infinity,
-            margin: const EdgeInsets.all(5),
-            padding: const EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: mainColor,
-              borderRadius: BorderRadius.circular(15),
-            ),
-
-            child: Column(
-              children: [
-                Row(
-                  spacing: 8,
-                  children: [
-                    Icon(
-                      Icons.vertical_align_top,
-                      color: Colors.white,
-                      size: 32,
-                    ),
-                    Text(
-                      "Pin Chat",
-                      style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Expanded(child: SizedBox()),
-                    Switch(
-                      value: notificationVal,
-                      onChanged: (notificationVal) {},
-                      activeColor: Colors.white, // لون الدائرة (thumb) وهي On
-                      activeTrackColor: secondaryColor, // لون الخلفية وهي On
-                      inactiveThumbColor:
-                          Colors.grey.shade400, // لون الدائرة وهي Off
-                      inactiveTrackColor:
-                          Colors.grey.shade300, // لون الخلفية وهي Off
-                      trackOutlineColor: MaterialStateProperty.all(
-                        Colors.transparent,
-                      ),
-                    ),
-                  ],
+          // pin chat
+          Obx(
+            () => settingsTile(
+              icon: Icons.vertical_align_top,
+              text: "Pin Chat",
+              trailing: Switch(
+                value: pinChatVal.value,
+                onChanged: (v) => pinChatVal.value = v,
+                activeColor: Colors.white,
+                activeTrackColor: secondaryColor,
+                inactiveThumbColor: Colors.grey.shade400,
+                inactiveTrackColor: Colors.grey.shade300,
+                trackOutlineColor: MaterialStateProperty.all(
+                  Colors.transparent,
                 ),
-              ],
+              ),
             ),
           ),
-          InkWell(
+          // block user
+          settingsTile(
+            icon: Icons.report,
+            text: "Block $name",
+            textColor: Colors.red,
             onTap: () {
               Get.snackbar("Notification", "$name is blocked");
             },
-            child: Container(
-              height: 100,
-              width: double.infinity,
-              margin: const EdgeInsets.all(5),
-              padding: const EdgeInsets.all(30),
-              decoration: BoxDecoration(
-                color: mainColor,
-                borderRadius: BorderRadius.circular(15),
-              ),
-
-              child: Column(
-                children: [
-                  Row(
-                    spacing: 8,
-                    children: [
-                      Icon(Icons.report, color: Colors.red, size: 32),
-                      Text(
-                        "Block $name",
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.red,
-                        ),
-                      ),
-                      Expanded(child: SizedBox()),
-                    ],
-                  ),
-                ],
-              ),
-            ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget settingsTile({
+    required IconData icon,
+    required String text,
+    Color? textColor,
+    Widget? trailing,
+    VoidCallback? onTap,
+  }) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 100,
+        width: double.infinity,
+        margin: const EdgeInsets.all(5),
+        padding: const EdgeInsets.all(30),
+        decoration: BoxDecoration(
+          color: mainColor,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          children: [
+            Icon(icon, color: textColor ?? Colors.white, size: 32),
+            const SizedBox(width: 8),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: textColor ?? Colors.white,
+              ),
+            ),
+            const Spacer(),
+            if (trailing != null) trailing,
+          ],
+        ),
       ),
     );
   }
